5cee8c530c1e00d0c05f437feef7f0fbe4331233
hillyjack <hilly.jackobson@gmail.com>
2017-11-26 16:45:18 +0200

    material on, http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatCardModule, MatToolbarModule } from '@angular/material';
+
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
